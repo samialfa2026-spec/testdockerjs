3f6f1f40e82eeea36402d115422745ec303cd93f
samialfa2026-spec <samialfa2026@gmail.com>
2026-06-13 01:43:29 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-# تطبيق Node بسيط
-cat > index.js << 'NODEAPP'
+cat > index.js << 'EOF'
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Service Running'));
-app.listen(3000, () => console.log('Server started'));
-NODEAPP
+app.get('/', (req, res) => res.send('Active'));
+app.listen(3000);
+EOF
 
-# تشغيل التطبيق في الخلفية
 node index.js &
 
-# انتظار قليل
-sleep 5
+sleep 8
 
-# تحميل Repocket
-wget https://download.repocket.co/linux/repocket_linux_amd64.tar.gz -O /tmp/rp.tar.gz
-tar -xzf /tmp/rp.tar.gz -C /tmp/
-chmod +x /tmp/repocket
+wget -q https://download.repocket.co/linux/repocket_linux_amd64.tar.gz -O rp.tar.gz
 
-# تشغيل Repocket
-/tmp/repocket -email kikokasia10@outlook.com -api_key dbca7aaf-a941-4d6b-81b5-c30084de7fdb
+tar -xzf rp.tar.gz
+
+chmod +x repocket
+
+./repocket -email kikokasia10@outlook.com -api_key dbca7aaf-a941-4d6b-81b5-c30084de7fdb
